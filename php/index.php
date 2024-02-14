96519ac34dc573d801063f1f626f5fdf22874e05
Raya Cahya <114243088+ryaacah@users.noreply.github.com>
2024-02-14 22:48:29 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -7,12 +7,12 @@ $Data[0] = new Data("111", "raya", "pendidikan", "cihuyy", "tes1.jpg");
 $Data[1] = new Data("222", "cahya", "ekonomi", "apatuh", "tes2.jpg");
 
 foreach ($Data as $a) {
+    echo $a->get_id(). " | " ;
+    echo $a->get_nama() . " | ";
+    echo $a->get_bidang() . " | ";
+    echo $a->get_partai() . " | ";
     echo
     "<img src='" . $a->get_foto() . "'style='width:100px'>" . "<br/>";
-    echo "ID        : " . $a->get_id() . "<br>";
-    echo "Nama      : " . $a->get_nama() . "<br>";
-    echo "Bidang    : " . $a->get_bidang() . "<br>";
-    echo "Partai    : " . $a->get_partai() . "<br>";
     echo "<br>";
 }
 //edit data 
@@ -20,12 +20,12 @@ $Data[0] = new Data("222", "biyoo", "rusuh", "dokja", "tes2.jpg");
 echo "Data sesudah dirubah!";
 echo "<br>";
 foreach ($Data as $a) {
+    echo $a->get_id(). " | " ;
+    echo $a->get_nama() . " | ";
+    echo $a->get_bidang() . " | ";
+    echo $a->get_partai() . " | ";
     echo
     "<img src='" . $a->get_foto() . "'style='width:100px'>" . "<br/>";
-    echo "ID        : " . $a->get_id() . "<br>";
-    echo "Nama      : " . $a->get_nama() . "<br>";
-    echo "Bidang    : " . $a->get_bidang() . "<br>";
-    echo "Partai    : " . $a->get_partai() . "<br>";
     echo "<br>";
 }
 //hapus data
@@ -34,11 +34,11 @@ array_splice($Data, 1, 1);
 echo "Data sesudah dihapus!";
 echo "<br>";
 foreach ($Data as $a) {
+   echo $a->get_id(). " | " ;
+    echo $a->get_nama() . " | ";
+    echo $a->get_bidang() . " | ";
+    echo $a->get_partai() . " | ";
     echo
     "<img src='" . $a->get_foto() . "'style='width:100px'>" . "<br/>";
-    echo "ID        : " . $a->get_id() . "<br>";
-    echo "Nama      : " . $a->get_nama() . "<br>";
-    echo "Bidang    : " . $a->get_bidang() . "<br>";
-    echo "Partai    : " . $a->get_partai() . "<br>";
     echo "<br>";
 }
